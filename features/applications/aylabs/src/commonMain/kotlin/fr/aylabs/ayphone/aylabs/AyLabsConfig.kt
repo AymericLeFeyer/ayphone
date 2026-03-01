fccbfd709b1ebcd1e27afbf7f960dd60f1d3c39a
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2026-03-01 10:01:35 +0100

    feat(app): use api

--- features/applications/aylabs/src/commonMain/kotlin/fr/aylabs/ayphone/aylabs/AyLabsConfig.kt
+++ features/applications/aylabs/src/commonMain/kotlin/fr/aylabs/ayphone/aylabs/AyLabsConfig.kt
@@ -1,7 +1,7 @@
 package fr.aylabs.ayphone.aylabs
 
 object AyLabsConfig {
-    const val YOUTUBE_STATS_URL = "https://aylabs.fr/youtube-stats.json"
+    const val YOUTUBE_STATS_URL = "https://api.aymeric.lefeyer.fr/api/youtube-stats"
     const val YOUTUBE_CHANNEL_URL = "https://www.youtube.com/@ay_labs"
     const val WEBSITE_URL = "https://aylabs.fr/"
     const val DOCS_URL = "https://docs.aylabs.fr/"
